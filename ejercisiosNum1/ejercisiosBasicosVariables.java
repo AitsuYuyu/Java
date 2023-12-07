d29d24d3ae75b017ae2f1205ef1fd6e07dded005
AitsuYuyu <136203413+AitsuYuyu@users.noreply.github.com>
2023-12-06 21:16:52 -0500

    feat: :sparkles: add archive with code of activity "variables"

---
+++ ejercisiosNum1/ejercisiosBasicosVariables.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercisiosnum1;
+
+/**
+ *
+ * @author heinn
+ */
+public class EjercisiosNum1 {
+
+    public static void main(String[] args) {
+        // Ejercicio 1: Escribe la declaración para cada una de las variables y asígnales un valor inicial en la propia declaración de variable.
+        // a) Variables enteras: p, q 
+//        int p = 5;
+//        int q = 28;
+//            System.out.println(p);
+//            System.out.println(q);
+        // b) Variables float: x, y, z
+//        float x = 4.2f;
+//        float y = 2.9f;
+//        float z = 12.7f;
+//              System.out.println(x);
+//              System.out.println(y);
+//              System.out.println(z);
+
+        // Variables carácter: a, b, c
+//        char a = 'a';
+//        char b = 'b';
+//        char c = 'c';
+//                System.out.println(a);
+//                System.out.println(b);
+//                System.out.println(c);
+        // d) Variables double: raiz1, raiz2 
+//        double raiz1 = 12d;
+//        double raiz2 = 9d;
+//                System.out.println(raiz1);
+//                System.out.println(raiz2);
+        // e) Variable entera larga: contador
+//        long contador = 0;
+//                System.out.println(contador);
+        //  f) Variable entera corta: indicador
+//        short indicador = 40;
+//                System.out.println(indicador);
+        // g) Variable entera: indice 
+//        int indice = 456;
+//                System.out.println(indice);
+        // h) Variables double: precio, precioFinal 
+//        double precio = 12340d;
+//        double precioFinal = 20400d;
+//                  System.out.println(precio + precioFinal);
+        // i) Variables carácter: car1, car2
+//        char car1 = 'M';
+//        char car2 = 'A';
+//                   System.out.println(car1);
+//                   System.out.println(car2);
+        //  j) Variable tipo byte: valor
+//        byte valor = 9;
+//                    System.out.println(valor);
+    }
+}
