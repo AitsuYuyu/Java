66cc382ad281cec502c026331060842e9f5473ed
AitsuYuyu <136203413+AitsuYuyu@users.noreply.github.com>
2023-12-11 11:39:51 -0500

    feat: :sparkles: add and update one exercise from strings

--- ejercisiosNum6/cadenas.java
+++ ejercisiosNum6/cadenas.java
@@ -1,5 +1,31 @@
 package ejercisiosNum6;
 
 public class cadenas {
-    
+    // Leer una frase y encontrar la palabra de mayor longitud. El programa debe imprimir la palabra como el número de caracteres de la misma
+    // BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+    //     int i, c=0;
+    //     String cadena, palabra="", mostrar = null;
+
+    //     System.out.println("Ingrese la cadena: ");
+    //     cadena=br.readLine();
+    //     cadena+=" ";
+
+    //     for(i=0;i<cadena.length();i++){
+    //         palabra=palabra+cadena.charAt(i);
+    //         if(cadena.charAt(i)==' '){
+    //             if(palabra.length()-1>c){
+    //                 c=palabra.length()-1;
+    //                 mostrar=palabra.trim();
+    //             }
+    //             palabra="";
+    //         } 
+    //     }
+    //     System.out.println("La palabra de mayor longitud es: "+mostrar);
+    //     System.out.println("Su longitud es: "+c);
+    // }
+
+
+    // Ingresar una cadena y determinar cuantas palabras se encuentran en la cadena.  Cada palabra se separa por medio de un espacio en blanco.
+
 }
